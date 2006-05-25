9f5205da4e081a9aab8948c85121153d5b041aef
tdb01r <tdb01r@acc99762-c6fb-0310-be58-e3f4808a066b>
2006-05-25 15:53:58 +0000

    * Missed main file
    git-svn-id: https://svn.eprints.org/iar/trunk/celestial-2@212 acc99762-c6fb-0310-be58-e3f4808a066b

---
+++ lib/Celestial.pm
@@ -0,0 +1,17 @@
+package Celestial;
+
+=head1 NAME
+
+Celestial - A mirroring tool for OAI-PMH repositories
+
+=head1 DESCRIPTION
+
+TODO
+
+=cut
+
+use vars qw( $VERSION );
+
+$VERSION = 2.0;
+
+use Celestial::DBI;
